4050cf8255ec6d21b8571bd5f122b1a6c1e99a37
Fezinn <felipemezzomofranca@alunos.utfpr.edu.br>
2025-10-23 17:51:03 -0300

    Adicionando alguns includes que serão importantes; Função que puxa o menu principal do game e lê a opção que será selecionada; Definida uma função simples que puxa uma linha pra quando for necessário "dar um enter" no jogo; Puxando na main e um printf que printa "fim" só para um debug mais compreensível.

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+void menuPrincipal(int opcao){
+    do{
+        linha();
+        printf("           JOGO DA MEMORIA\n");
+        linha();
+        printf("1 - Novo Jogo\n");
+        printf("2 - Carregar Jogo (indisponivel)\n");
+        printf("3 - Sair\n");
+        linha();
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+    } while (opcao < 1 || opcao > 3);
+}
+
+void linha(){
+    puts("-------------------------------------------------\n");
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int opcao;
+    menuPrincipal(opcao);
+
+    printf("fim");
     return 0;
 }
